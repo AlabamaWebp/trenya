e62b8da97f125e2ad874e6cfeb0bd9f2f9bbf060
AlabamaWebp <dimyrp@yandex.ru>
2024-01-20 14:36:41 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import items from './items';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      {items.map((item) => {
+        return (
+          <div className='item'>
+            <div>
+              <h1>{item.name}</h1>
+              <ul>
+                {item.steps.map((step) => {
+                  return <li className='step'>{step}</li>
+                })}
+              </ul>
+            </div>
+
+          </div>
+        )
+      })}
     </div>
   );
 }
